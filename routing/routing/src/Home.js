82afa327b01b22478132175ed29a9ce1bec50504
dhruvi6071 <dhruvi6071@gmail.com>
2024-06-17 01:45:51 +0530

    Link-element-to-stop-reloading-page

--- routing/routing/src/Home.js
+++ routing/routing/src/Home.js
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
+
 function HomePage() {
   return (
     <>
       <h1>My home page</h1>
       <p>
-        Go to <a href="/product">product page</a>
+        Go to <Link to="/product">product page</Link>
       </p>
     </>
   );
