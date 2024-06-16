c319e942e9761259c7bd15c9f22f8e372258ba98
dhruvi6071 <dhruvi6071@gmail.com>
2024-06-17 01:00:43 +0530

    product-page

--- routing/routing/src/App.js
+++ routing/routing/src/App.js
@@ -1,10 +1,11 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import HomePage from "./Home";
+import ProductsPage from "./Products";
 
 //http://example.com/products
 const router = createBrowserRouter([
   {path: '/', element: <p><HomePage/></p> },
-  {}
+  {path: '/product', element:<ProductsPage />}
 ])
 
 function App() {
---
+++ routing/routing/src/Products.js
@@ -0,0 +1,5 @@
+function ProductsPage() {
+    return <h1>The product page </h1>
+}
+
+export default ProductsPage;
